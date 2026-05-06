62aab59012f20f53e564db850ede66666aa4de7f
yogadewi2014-droid <yogadewi2014@gmail.com>
2026-05-06 21:50:59 +0800

    Update prisma.ts

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-export default prisma
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default prisma;
