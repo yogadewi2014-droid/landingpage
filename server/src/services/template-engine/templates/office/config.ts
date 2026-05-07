eeeda4f48e080f5310bb61b38f8b135ba9c52d9a
yogadewi2014-droid <yogadewi2014@gmail.com>
2026-05-07 20:42:54 +0800

    Delete server/src/services/template-engine/templates/office/config.ts

--- server/src/services/template-engine/templates/office/config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  name: "Office Simple",
-  category: "office",
-  defaultStyles: {
-    primaryColor: "#1a365d",
-    backgroundColor: "#ffffff",
-    fontFamily: "Arial, sans-serif"
-  }
-}
